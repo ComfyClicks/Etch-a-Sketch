6157bcf9c9a014979eacb90325ba246e714f51fa
ComfyClicks <79934542+ComfyClicks@users.noreply.github.com>
2024-05-07 14:04:17 -0400

    added reset functionality

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 let squares = 16;
 
-
+function setPad() {
   for (let i = 0; i < squares * squares; i++) {
-    let square = document.createElement('div');
+    const square = document.createElement('div');
     square.style.width = `${100 / squares}%`;
     square.style.height = `${100 / squares}%`;
     document.getElementById('container').appendChild(square);
 
     square.addEventListener('mouseover', (e) => {
-      e.target.style.backgroundColor = 'red';
+      e.target.style.backgroundColor = 'orange';
     })
   }
+};
 
+
+function resetPad() {
+  const reset = document.getElementById('reset');
+  const pad = document.getElementById('container');
+
+  reset.addEventListener('click', () => {
+
+    pad.innerText = '';
+
+    for (let i = 0; i < squares * squares; i++) {
+      const square = document.createElement('div');
+      square.style.width = `${100 / squares}%`;
+      square.style.height = `${100 / squares}%`;
+      pad.appendChild(square);
+
+      square.addEventListener('mouseover', (e) => {
+        e.target.style.backgroundColor = 'orange';
+      })
+    }
+  })
+};
+
+setPad();
+resetPad();
