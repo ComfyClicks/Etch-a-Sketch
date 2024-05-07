d0491bb43237b51fb488c5d2dcd7156ac8ca4d7a
ComfyClicks <79934542+ComfyClicks@users.noreply.github.com>
2024-05-07 16:22:13 -0400

    added random color functionality

--- app.js
+++ app.js
@@ -35,5 +35,31 @@ function resetPad() {
   })
 };
 
+function randomColor() {
+  const random = document.getElementById('random');
+  const pad = document.getElementById('container');
+
+  random.addEventListener('click', () => {
+    pad.innerText = '';
+
+    for (let i = 0; i < squares * squares; i++) {
+      const square = document.createElement('div');
+      square.style.width = `${100 / squares}%`;
+      square.style.height = `${100 / squares}%`;
+      pad.appendChild(square);
+
+      square.addEventListener('mouseover', (e) => {
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      })
+    }
+  })
+};
+
+
 setPad();
 resetPad();
+randomColor();
